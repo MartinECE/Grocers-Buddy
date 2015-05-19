3eed2e493765d25ebd0529e19f2124bc432f1dff
MartinECE <martin.eagan.charles@gmail.com>
2015-05-19 13:45:37 +0800

    v64

--- www/index.php
+++ www/index.php
@@ -32,13 +32,14 @@
 		
 		  <link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
         
-        <title>GROCER'S BUDDY</title>
+        
+        <title>Grocer's Buddy</title>
     </head>
-    <body>
+    <body onload="loaded()">
     
         <a class="gb-menu-items" href="add.php"><div style="background-color: #FFFFFF; color:#727272;"><i class="fa fa-plus"></i> ADD ITEM</div></a>
         <a class="gb-menu-items" href="available.php"><div style="background-color: #0288D1; color:#b3e5fc;"><i class="fa fa-calendar"></i> SHOP</div></a>
-        <a class="gb-menu-items" href="list.php"><div style="background-color: #FFFFFF; color:#727272;">LIST CATEGORY</div></a>
+        <a class="gb-menu-items" href="list.php"><div style="background-color: #FFFFFF; color:#727272;"><i class="fa fa-shopping-cart"></i> LIST CATEGORY</div></a>
         <button id="cleardata" style="background-color: #607d8b; color:#cfd8dc;">CLEAR DATA</button>
 
         <script type="text/javascript" src="js/index.js"></script>
